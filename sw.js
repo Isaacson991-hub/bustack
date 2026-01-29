08d21cb8ff42ce16a3423556bdcde4aba27c6ae1
Isaacson991-hub <cabralalvesisaac2@gmail.com>
2026-01-29 19:17:39 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-const CACHE_NAME = "bustack-v1";
-
-const FILES_TO_CACHE = [
+const CACHE="bustack-v2";
+const FILES=[
   "./",
   "./index.html",
   "./manifest.json",
+  "./linhas.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
